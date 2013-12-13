2c81a1c1ecab9e408463f0402639ec8e0c9f44a3
Anders Åslund <anders.aslund@me.com>
2013-12-13 10:56:46 +0100

    Added tags to logs

--- lib/elasticsearch.rb
+++ lib/elasticsearch.rb
@@ -11,43 +11,49 @@ require 'base64'
 class ElasticSearch
 
     def initialize(step_mother, io, options)
-      p "Initation ElasticSearch formatter"
       @job = ENV['JOB']
       @build = ENV['BUILD']
-      @es = Elasticsearch::Client.new hosts: ['http://192.168.2.47:9200'], reload_connections: true
+      p "Initation ElasticSearch formatter for #{@job} : #{@build}"
+      @es = Elasticsearch::Client.new hosts: ['194.218.9.53:9200'], reload_connections: true
 
     end
 
     def before_feature(feature)
       @feature = feature.title
+    
+      tagArr = Array.new
+      feature.source_tag_names.each do |tag|
+        tagArr.push(tag:tag)
+      end
+      @feature_tags = tagArr
     end
 
     def scenario_name(keyword, name, file_colon_line, source_indent)
-      puts "#{name}"
-
-         @scenario =  name
+      p "#{name}"
+      @scenario =  name
     end
 
     def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background, file_colon_line)
       if @in_background == true
         # do nothing. background gets reported as step anyway
       else
-         step_name = step_match.format_args(lambda{|param| "*#{param}*"})
+        step_name = step_match.format_args(lambda{|param| "*#{param}*"})
+        p "  #{step_name}"
 
         @es.index index: 'cucumber-plugin',
-         type:  'step',
-         body: {
-          job:@job,
-          build:@build,
-          feature:@feature,
-          scenario:@scenario,
-          keyword: keyword,
-          step_name: step_name,
-          status:status,
-          exception:exception,
-          timestamp:Time.now.to_i
+          type:  'step',
+          body: {
+            job:@job,
+            build:@build,
+            feature:@feature,
+            scenario:@scenario,
+            keyword: keyword,
+            step_name: step_name,
+            status:status,
+            exception:exception,
+            timestamp:Time.now.to_i,
+            tags:@feature_tags
          }
       end
     end
-
 end
